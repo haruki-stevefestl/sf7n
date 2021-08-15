b81f4606c867caa6330f632aa0d335835862e7ba
haruki-taka8 <stevefestl@gmail.com>
2021-08-15 17:59:47 +0800

    Multiple changes:
    * Implement undo
    * Optimize Add-Row by using InsertRange instead of For-Insert loop

--- Resources/Functions/Edit.ps1
+++ Resources/Functions/Edit.ps1
@@ -1,14 +1,82 @@
-function Add-Row ($Data, $Action, $At, $Count, $Format, $Header) {
+function Update-Grid {
+    $rows.Undo.IsEnabled   = $true
+    $rows.Commit.IsEnabled = $true
+    $rows.Grid.ItemsSource = $csv
+    $rows.Grid.Items.Refresh()
+}
+
+function Add-Undo ($UndoStack, $Data, $Operation, $At, $OldRow, $Count) {
+    $ToAdd = [PSCustomObject] @{
+        Action   = $Operation
+        RowIndex = $At
+        Original = $OldRow.PSObject.Copy()
+        Count    = $Count
+    }
+
+    if ($UndoStack) {
+        # Add to undo stack if any one of requirements satisfied
+        #   - row value changed
+        #   - row index changed
+        $ToAdd.Original.PSObject.Properties.ForEach({
+            if ($_.Value -ne $UndoStack[-1].Original.($_.Name)) {
+                $UndoStack.Add($ToAdd)
+                return $UndoStack
+            }
+        })
+
+        if ($ToAdd.RowIndex -ne $UndoStack[-1].RowIndex) {
+            $UndoStack.Add($ToAdd)
+            return $UndoStack
+        }
+        
+    } else {
+        # @($null, ) since PSv5 (only) cannot handle single element
+        # https://github.com/PowerShell/PowerShell/issues/2208
+        [Collections.ArrayList] $UndoStack = @($null, $ToAdd)
+        return $UndoStack
+    }
+}
+
+function Invoke-Undo ($UndoStack, $Data) {
+    if ($UndoStack) {
+        $Last = $UndoStack[-1]
+
+        switch -regex ($Last.Action) {
+            'Change' {
+                $Data[$Last.RowIndex] = $Last.Original
+            }
+
+            'Remove' {
+                # Readd row
+                if ($Data) {
+                    $Data.Insert($Last.RowIndex, $Last.Original)
+                } else {
+                    [Collections.ArrayList] $Data = @($Last.Original)   
+                }
+            }
+
+            'Insert(Last|Above|Below)' {
+                if ($Last.Action -eq 'InsertBelow') {$Offset = $Last.Conut}
+                $Data.RemoveRange($Last.RowIndex+$Offset, $Last.Count)
+            }
+        }
+
+        $UndoStack.RemoveAt($UndoStack.Count-1)
+    }
+    return $UndoStack, $Data
+}
+
+function Add-Row ($Data, $Action, $At, $Count, $Header, $Format) {
     # $Format and $Header only for $Action == 'InsertLast'
     # Prepare blank template for inserting
     $RowTemplate = [PSCustomObject] @{}
     $Header.Foreach({$RowTemplate | Add-Member NoteProperty $_ ''})
     
     if ($Action -eq 'InsertLast') {
+        $Now = Get-Date
         for ($i = 0; $i -lt $Count; $i++) {
             # Expand <x> noation
-            $Now = Get-Date
-            $ThisRow = $RowTemplate.PsObject.Copy()
+            $ThisRow = $RowTemplate.PSObject.Copy()
             $ThisRow.($Header[0]) = $Format -replace
                 '<D>', $Now.ToString('yyyyMMdd') -replace
                 '<T>', $Now.ToString('HHmmss')   -replace
@@ -24,15 +92,22 @@ function Add-Row ($Data, $Action, $At, $Count, $Format, $Header) {
     } else {
         # InsertAbove/InsertBelow
         # Max & Min to prevent under/overflowing
-        for ($i = 0; $i -lt $Count; $i++) {
-            $Data.Insert(
-                [Math]::Max(0, [Math]::Min($At,$Data.Count)),
-                $RowTemplate.PSObject.Copy()
-            )
-        }
+        $Data.InsertRange(
+            [Math]::Max(0, [Math]::Min($At, $Data.Count)),
+            @($RowTemplate)*$Count
+        )
     }
 
-    $rows.Commit.IsEnabled = $true
-    $rows.Grid.ItemsSource = $Data
-    $rows.Grid.Items.Refresh()
+    # Process undo
+    $Parameters = @{
+        UndoStack = $undo
+        Data      = $csv
+        Operation = $Action
+        At        = $At
+        OldRow    = ''
+        Count     = $Count
+    }
+    [Collections.ArrayList] $script:undo = Add-Undo @Parameters
+
+    return $Data
 }
--- Resources/Handlers/Edit.ps1
+++ Resources/Handlers/Edit.ps1
@@ -1,31 +1,75 @@
 # Enter edit mode
-$rows.Grid.Add_BeginningEdit({$rows.Status.Text = 'Editing'})
+$rows.Grid.Add_BeginningEdit({
+    $rows.Status.Text = 'Editing'
+    $script:oldRow = $Args[1].Row.Item
+})
 
 # Change rows (add/remove)
-$rows.Grid.Add_CellEditEnding({$rows.Commit.IsEnabled = $true})
+$rows.Grid.Add_CellEditEnding({
+    $Parameters = @{
+        UndoStack = $undo
+        Data      = $csv
+        Operation = 'Change'
+        At        = $csv.IndexOf($oldRow)
+        OldRow    = $oldRow
+        Count     = 1
+    }
 
+    [Collections.ArrayList] $script:undo = Add-Undo @Parameters
+    $rows.Undo.IsEnabled = $true
+    $rows.Commit.IsEnabled = $true
+})
+
+$rows.Undo.Add_Click({
+    $script:undo, $script:csv = Invoke-Undo $undo $csv
+    Update-Grid
+
+    if (!$undo) {$rows.Undo.IsEnabled = $false}
+})
+
+# $rows.InsertLast.Add_Click({ Add-Row 'InsertLast'; Update-Grid})
+# $rows.InsertAbove.Add_Click({Add-Row 'InsertAbove'; Update-Grid})
+# $rows.InsertBelow.Add_Click({Add-Row 'InsertBelow'; Update-Grid})
 $rows.InsertLast.Add_Click({
-    Add-Row $csv 'InsertLast' 0 $config.AppendCount $config.AppendFormat $csvHeader
+    $csv = Add-Row $csv 'InsertLast' 0 $config.AppendCount $csvHeader $config.AppendFormat
+    Update-Grid
 })
 
 $rows.InsertAbove.Add_Click({
     $At = $csv.IndexOf($rows.Grid.SelectedItem)
     $Count = $rows.Grid.SelectedItems.Count
 
-    if ($Count -gt 0) {Add-Row $csv 'InsertAbove' $At $Count}
+    if ($Count -gt 0) {
+        $csv = Add-Row $csv 'InsertAbove' $At $Count $csvHeader
+        Update-Grid
+    }
 })
 
 $rows.InsertBelow.Add_Click({
     $At = $csv.IndexOf($rows.Grid.SelectedItem)
     $Count = $rows.Grid.SelectedItems.Count
 
-    if ($Count -gt 0) {Add-Row $csv 'InsertAbove' ($At+$Count) $Count}
+    if ($Count -gt 0) {
+        $csv = Add-Row $csv 'InsertAbove' ($At+$Count) $Count $csvHeader
+        Update-Grid
+    }
 })
 
 $rows.RemoveSelected.Add_Click({
-    $rows.Grid.SelectedItems.ForEach{$csv.Remove($_)}
-    $rows.Grid.ItemsSource = $csv
-    $rows.Grid.Items.Refresh()
+    $rows.Grid.SelectedItems.ForEach({
+        # Process undo
+        $Parameters = @{
+            UndoStack = $undo
+            Data      = $csv
+            Operation = 'Remove'
+            OldRow    = $_
+            At        = $csv.IndexOf($_)
+            Count     = 1
+        }
+        [Collections.ArrayList] $script:undo = Add-Undo @Parameters
+        $csv.Remove($_)
+    })
+    Update-Grid
 })
 
 # Commit CSV
